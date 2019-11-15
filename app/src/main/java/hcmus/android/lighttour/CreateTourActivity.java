3da7f8e816ede9d3bd4cb06943134a296f34ff04
tvp-github <tavinhphuc.ctg@gmail.com>
2019-11-15 10:01:44 +0700

    + Init create tour activity

---
+++ app/src/main/java/hcmus/android/lighttour/CreateTourActivity.java
@@ -0,0 +1,14 @@
+package hcmus.android.lighttour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateTourActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.create_tour);
+    }
+}
